62d3292dc9edd6aa38b41690051052ddfb5db280
Mufamadi Anwani Nicolas <mufamadianwani@gmail.com>
2023-03-28 15:33:49 +0200

    Added user authentication

---
+++ components/middleware/auth.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = async(req,res,next) => {
+
+    const bearerToken = req.headers.authorization;
+    
+    if(bearerToken){
+        try {
+            const token = bearerToken.split(" ")[1];
+            const decryptedToken = jwt.verify(token,process.env.SECRET)
+            res.status(200).send({data: decryptedToken});
+           // next();
+            
+        } catch (error) {
+            res.status(401).send({err: "User not authenticated"});
+        }
+    }else{
+        res.status(401).send({err: "User not authenticated"});
+    }
+
+}
