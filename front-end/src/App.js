4c3a3dbd0865849a48d82b852d2754f6f3248ff1
Mufamadi Anwani Nicolas <mufamadianwani@gmail.com>
2023-03-28 15:38:11 +0200

    created react app and add header component

---
+++ front-end/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import DrawerBar from '../src/components/header/drawer'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+         <DrawerBar />
+      </header>
+    </div>
+  );
+}
+
+export default App;
