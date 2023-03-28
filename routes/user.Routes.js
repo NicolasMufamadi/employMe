d3dde386a01e1372e7f7e80fd8ecb76c8cfb6b7a
Mufamadi Anwani Nicolas <mufamadianwani@gmail.com>
2023-03-28 15:36:38 +0200

    Added users routes end points

---
+++ routes/user.Routes.js
@@ -0,0 +1,19 @@
+const router = require('express').Router();
+
+const auth = require('../components/middleware/auth');
+const add = require('../components/user/add');
+const _delete = require('../components/user/delete');
+const getAll = require('../components/user/getAll');
+const getById = require('../components/user/getById');
+const login = require('../components/user/login');
+const update = require('../components/user/update');
+
+router.post('/',add);
+router.post('/login',login);
+router.get('/auth',auth)
+router.get('/',getAll);
+router.get('/:user_id',auth,getById);
+router.put('/:user_id',auth,update);
+router.delete('/:user_id',auth,_delete);
+
+module.exports = router;
