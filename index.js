c18fe139e36095f04c9d9773a950c452cb87d8fd
Mufamadi Anwani Nicolas <mufamadianwani@gmail.com>
2023-05-22 23:08:45 +0200

    added api x-key to the http headers

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const http = require('http');
+
 require('dotenv').config();
 
 const userRoutes = require('./routes/user.Routes');
@@ -14,4 +16,13 @@ app.use(express.json());
 //routes
 app.use('/user/',userRoutes);
 
-app.listen(process.env.PORT,()=> console.log(`Server running on https://localhost:${process.env.PORT}`));
+app.listen(process.env.PORT,()=>{
+    console.log(`Server running on https://localhost:${process.env.PORT}`)
+    var options = {
+        port: '4444',
+        host: 'localhost'
+    }
+    var request = http.request(options);
+
+    request.setHeader('X-Api-Key','json')
+});
