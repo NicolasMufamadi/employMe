2b1b2fdb4197eefe80defea85a783d08dab51ea3
Mufamadi Anwani Nicolas <mufamadianwani@gmail.com>
2023-05-24 22:27:36 +0200

    added persistGate which delay UI render until persisted data is available in the store

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,11 +1,13 @@
+import './index.css';
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/es/integration/react';
 
-import './index.css';
 import App from './App';
-import store from './store/store';
+import { store,persistor } from './store/store';
 
 
 
@@ -14,7 +16,9 @@ root.render(
  
     <BrowserRouter>
       <Provider store={store}>
-        <App />
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
       </Provider>
     </BrowserRouter>
 
