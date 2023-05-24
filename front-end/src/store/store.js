69b3d18a30a0fbad249dde67a1665295b390927a
Mufamadi Anwani Nicolas <mufamadianwani@gmail.com>
2023-05-24 22:29:57 +0200

    added redux-persist to persist user data in the local storage

--- front-end/src/store/store.js
+++ front-end/src/store/store.js
@@ -1,8 +1,25 @@
 import { configureStore } from '@reduxjs/toolkit';
+import  storage  from 'redux-persist/lib/storage';
+import { persistReducer, persistStore } from 'redux-persist';
+import thunk from 'redux-thunk';
+
 import userSliceReducer from './slices/userSlice';
 
-export default configureStore({
+//configuration to persist the state store into local storage 
+const persistConfig = {
+    key: 'root',
+    storage
+}
+
+//enhanced reducer with configuration to persist the userSliceReducer
+const persistedReducer = persistReducer(persistConfig,userSliceReducer);
+
+export const store = configureStore({
     reducer: {
-        user: userSliceReducer,
-    }
+        user: persistedReducer,
+    },
+    middleware: [thunk]
 })
+
+//persist the store 
+export const persistor = persistStore(store);
