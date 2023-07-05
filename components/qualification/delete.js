0aff645cc0faab4195b3a396d879c8aea1b30dd5
Mufamadi Anwani Nicolas <mufamadianwani@gmail.com>
2023-07-05 23:58:10 +0200

    return deleted rows

--- components/qualification/delete.js
+++ components/qualification/delete.js
@@ -2,13 +2,13 @@ const db = require('../../database/connection');
 
 module.exports = ((req,res) =>{ 
 
-    const qualification_id = req.params.user_id;
+    const qualification_id = req.params.qualification_id;
 
-    db.query('DELETE FROM qualifications WHERE qualification_id = ($1)',[qualification_id])
+    db.query('DELETE  FROM qualifications WHERE qualification_id = ($1) RETURNING *',[qualification_id])
     .then(response =>{
         res.status(200).send(response);
     }).catch(err => {
-        res.send(404).send(err);
+        res.status(404).send(err);
     })
 
 })
