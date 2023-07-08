7154887f545de4ee47c4b55dc6bd5d13c54c68e6
Mufamadi Anwani Nicolas <mufamadianwani@gmail.com>
2023-07-08 23:57:51 +0200

    added province and street_no to address schema

--- components/address/add.js
+++ components/address/add.js
@@ -2,16 +2,18 @@ const db = require('../../database/connection');
 
 module.exports = ((req,res) => {;
 
-    const { user_id,street_name,feature,suburb,city,zip_code } = req.body;
+    const { user_id,street_name,feature,suburb,city,zip_code,province,street_no } = req.body;
 
     db.query(
-        "INSERT INTO address(user_id,street_name,feature,suburb,city,zip_code) VALUES($1,$2,$3,$4,$5,$6) RETURNING *",[
+        "INSERT INTO address(user_id,street_name,feature,suburb,city,zip_code,province,street_no) VALUES($1,$2,$3,$4,$5,$6,$7,$8) RETURNING *",[
            user_id,
            street_name,
            feature,
            suburb,
            city,
-           zip_code   
+           zip_code,
+           province,
+           street_no,  
         ]
     ).then(response => {
         res.status(201).send(response.rows[0]);
