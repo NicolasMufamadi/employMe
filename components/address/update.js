07c2e45d2495a9be634530208d03070a8d88e4f4
Mufamadi Anwani Nicolas <mufamadianwani@gmail.com>
2023-07-09 01:16:45 +0200

    added field in address schema

--- components/address/update.js
+++ components/address/update.js
@@ -3,19 +3,21 @@ const db = require('../../database/connection');
 module.exports = ( async (req,res) => {
 
     const address_id = req.params.address_id;
-    const { user_id,street_name,feature,suburb,city,zip_code } = req.body;
+    const { user_id,street_no,street_name,feature,suburb,city,zip_code,province } = req.body;
 
     try {
      
         const getAddress = await db.query("SELECT * FROM address WHERE user_id = ($1)",[user_id])
         const address = getAddress.rows[0];
        console.log(address)
-        db.query("UPDATE address SET street_name = ($1), feature = ($2), suburb = ($3), city = ($4), zip_code = ($5) WHERE address_id = ($6)",[
+        db.query("UPDATE address SET street_no = ($1), street_name = ($2), feature = ($3), suburb = ($4), city = ($5), zip_code = ($6), province =($7) WHERE address_id = ($8)",[
+            street_no,
             street_name,
             feature,
             suburb,
             city,
             zip_code,
+            province,
             address_id
         
         ]).then( response => {
