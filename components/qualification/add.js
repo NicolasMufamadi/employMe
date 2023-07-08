6a88c3a0743138ab3c972e7ba719d5a166b6ea8d
Mufamadi Anwani Nicolas <mufamadianwani@gmail.com>
2023-07-09 01:17:13 +0200

    added field in qualification schema

--- components/qualification/add.js
+++ components/qualification/add.js
@@ -2,11 +2,12 @@ const db = require('../../database/connection');
 
 module.exports = ((req,res) => {
 
-    const {user_id,qualification_status,institution_name,study_field,study_type,starting_date,ending_date} = req.body;
+    const {user_id,qualification_status,qualification_type,institution_name,study_field,study_type,starting_date,ending_date} = req.body;
 
-    db.query("INSERT INTO qualifications(user_id,qualification_status,institution_name,study_field,study_type,starting_date,ending_date) VALUES($1,$2,$3,$4,$5,$6,$7) RETURNING*",[
+    db.query("INSERT INTO qualifications(user_id,qualification_status,qualification_type,institution_name,study_field,study_type,starting_date,ending_date) VALUES($1,$2,$3,$4,$5,$6,$7,$8) RETURNING*",[
         user_id,
         qualification_status,
+        qualification_type,
         institution_name,
         study_field,
         study_type,
