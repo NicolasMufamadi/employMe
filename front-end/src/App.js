00444163242da93cfeb77436997c2013609aed8b
Mufamadi Anwani Nicolas <mufamadianwani@gmail.com>
2023-07-31 00:07:06 +0200

    removed footer

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,28 +1,15 @@
 import './App.css';
 import Router from './routing/router';
 import Header from './components/header/header';
-import Footer from './components/footer/footer';
+//import Footer from './components/footer/footer';
 import ScrollTop from './components/scrolltop';
 
 function App() {
   return (
-    <div className="App">
-      <div>
-       <Header  />
-      </div>
-
-      <div className='body'>
+    <div className="App"> 
+        <Header  />
         <Router />
-          <ScrollTop />
-        
-      </div>
-
-      <div style={{marginBottom: '10rem'}}></div>
-
-      <div>
-        <Footer />
-      </div>
-  
+        <ScrollTop />
     </div>
   );
 }
