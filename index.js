bcc69ecdc6297f64de7f00d8a041aaae502e3f6d
Mufamadi Anwani Nicolas <mufamadianwani@gmail.com>
2023-08-11 15:33:11 +0200

    added routes

--- index.js
+++ index.js
@@ -14,10 +14,14 @@ app.use(express.json());
 const userRoutes = require('./routes/user.routes');
 const addressRoutes = require('./routes/address.routes');
 const academicRoutes = require('./routes/qualification.routes');
+const jobRoutes = require('./routes/job.routes');
+const applicatioRoutes = require('./routes/application.Routes');
 
 app.use('/user/',userRoutes);
 app.use('/address',addressRoutes);
 app.use('/qualification',academicRoutes);
+app.use('/job',jobRoutes);
+app.use('/application',applicatioRoutes)
 
 app.listen(process.env.PORT,()=>{
     console.log(`Server running on https://localhost:${process.env.PORT}`)
