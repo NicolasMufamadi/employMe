fad5b098852479f96d958fdba2f67c6470702fe8
Mufamadi Anwani Nicolas <mufamadianwani@gmail.com>
2023-08-11 15:34:05 +0200

    removed offset

--- components/user/getAll.js
+++ components/user/getAll.js
@@ -2,8 +2,10 @@ const db = require('../../database/connection');
 
 module.exports = async(req,res) => {
 
+    const offset = req.params.offset 
+
     try {
-        const users = await db.query("SELECT * FROM Users");
+        const users = await db.query("SELECT * FROM Users ORDER BY user_id LIMIT 4 OFFSET ($1)",[offset]);
         if(users.rowCount > 0){
             res.status(200).send(users.rows);
         }else{
