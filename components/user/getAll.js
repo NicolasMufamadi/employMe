6b96649e71181ac73ffdc9fc734b1706e79759fa
Mufamadi Anwani Nicolas <mufamadianwani@gmail.com>
2024-04-12 22:33:40 +0200

    remove offset from get all users api

--- components/user/getAll.js
+++ components/user/getAll.js
@@ -2,11 +2,12 @@ const db = require('../../database/connection');
 
 module.exports = async(req,res) => {
 
-    const offset = req.params.offset 
+   // const offset = req.params.offset 
 
     try {
-        const users = await db.query("SELECT * FROM Users ORDER BY user_id LIMIT 4 OFFSET ($1)",[offset]);
+        const users = await db.query("SELECT * FROM Users ");
         if(users.rowCount > 0){
+            console.log(users.rows)
             res.status(200).send(users.rows);
         }else{
            res.status(404).send({err: "No users"});  
