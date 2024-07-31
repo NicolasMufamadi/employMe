f328a98b9d2846c80153b10b88d24af44a57ff15
Mufamadi Anwani Nicolas <mufamadianwani@gmail.com>
2024-08-01 01:06:20 +0200

    made the get request dynamic

--- components/qualification/getAll.js
+++ components/qualification/getAll.js
@@ -3,7 +3,7 @@ const db = require('../../database/connection');
 module.exports = (async(req,res) => {
 
     try {
-        const q =await db.query('SELECT * FROM qualifications WHERE user_id = 3');
+        const q =await db.query('SELECT * FROM qualifications WHERE user_id = ($1)',[req.params.user_id]);
         res.send(q.rows)
     } catch (error) {
         res.send(error)
