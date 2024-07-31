115d3a0df58fd8ce8d4273e50bb6183afe07b421
Mufamadi Anwani Nicolas <mufamadianwani@gmail.com>
2024-08-01 01:06:54 +0200

    removed password from the jwt

--- components/user/login.js
+++ components/user/login.js
@@ -9,14 +9,27 @@ module.exports = async (req,res) => {
 
     try {
         const auth = await db.query("SELECT * FROM Users WHERE email = ($1)",[email]);
-        console.log(auth)
+
         if(auth.rows[0]){
+
             const isMatch = await bcrypt.compare(clearPassword,auth.rows[0].password);
-            console.log(isMatch)
+
+            const payLoad = {
+                user_id: auth.rows[0].user_id,
+                first_name: auth.rows[0].first_name,
+                middle_name: auth.rows[0].middle_name,
+                last_name: auth.rows[0].last_name,
+                number_phone: auth.rows[0].number_phone,
+                telephone: auth.rows[0].telephone,
+                email: auth.rows[0].email,
+                userrole: auth.rows[0].userrole, 
+                gender: auth.rows[0].gender,
+                organization: auth.rows[0].organization
+            }
             if(isMatch){
                 
-                const token = jwt.sign(auth.rows[0],process.env.SECRET,{
-                    expiresIn: '10min'
+                const token = jwt.sign(payLoad,process.env.SECRET,{
+                    expiresIn: '1hr'
                 })
                 res.status(200).send({data: auth.rows[0],token: token});
             }else{
