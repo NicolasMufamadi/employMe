7a5378715bfa1f8adf915ead3b50fb4cbb89b253
Mufamadi Anwani Nicolas <mufamadianwani@gmail.com>
2024-08-01 01:05:39 +0200

    changed the request parameter

--- components/address/update.js
+++ components/address/update.js
@@ -3,14 +3,11 @@ const db = require('../../database/connection');
 module.exports = ( async (req,res) => {
 
     const address_id = req.params.address_id;
-    const { user_id,street_no,street_name,feature,suburb,city,zip_code,province } = req.body;
+    const { street_no,street_name,feature,suburb,city,zip_code,province } = req.body;
 
     try {
-     
-        const getAddress = await db.query("SELECT * FROM address WHERE user_id = ($1)",[user_id])
-        const address = getAddress.rows[0];
-       console.log(address)
-        db.query("UPDATE address SET street_no = ($1), street_name = ($2), feature = ($3), suburb = ($4), city = ($5), zip_code = ($6), province =($7) WHERE address_id = ($8)",[
+
+        const update = await db.query("UPDATE address SET street_no = ($1), street_name = ($2), feature = ($3), suburb = ($4), city = ($5), zip_code = ($6), province =($7) WHERE address_id = ($8) RETURNING *",[
             street_no,
             street_name,
             feature,
@@ -20,11 +17,9 @@ module.exports = ( async (req,res) => {
             province,
             address_id
         
-        ]).then( response => {
-            res.status(200).send(response); 
-        }).catch(err =>{
-            res.status(400).send(err);
-        })
+        ])
+
+        res.status(200).send(update.rows[0])
 
     } catch (error) {
          console.log(error)       
