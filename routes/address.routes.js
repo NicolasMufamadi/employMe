83cb9b1a4b84c04eef1f637a72a43264a2f65408
Mufamadi Anwani Nicolas <mufamadianwani@gmail.com>
2024-08-01 01:12:35 +0200

    added remove address api

--- routes/address.routes.js
+++ routes/address.routes.js
@@ -3,9 +3,11 @@ const router = require('express').Router();
 const add = require('../components/address/add');
 const update = require('../components/address/update');
 const getUserAddress = require('../components/address/get.user.address');
+const removeAddress = require('../components/address/remove');
 
 router.post('/',add);
 router.get('/:user_id',getUserAddress);
 router.put('/:address_id',update);
+router.delete('/:address_id',removeAddress);
 
 module.exports = router;
