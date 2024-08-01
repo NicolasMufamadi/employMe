a89355803cf857623ed011027e8e8bb9a462dbe3
Mufamadi Anwani Nicolas <mufamadianwani@gmail.com>
2024-08-01 23:34:48 +0200

    added skills,work experience and online accounts

--- front-end/src/components/profile/myaccount.js
+++ front-end/src/components/profile/myaccount.js
@@ -5,8 +5,9 @@ import PersonIcon from '@mui/icons-material/Person';
 import HomeIcon from '@mui/icons-material/Home';
 import SchoolIcon from '@mui/icons-material/School';
 import IconButton  from '@mui/material/IconButton';
-
-
+import PsychologyIcon from '@mui/icons-material/Psychology';
+import WorkIcon from '@mui/icons-material/Work';
+import LanguageIcon from '@mui/icons-material/Language';
 
 export default function MyAccount(){
 
@@ -14,10 +15,10 @@ export default function MyAccount(){
 
     return(
        <Container>
-           <div style={{marginTop: '100px',display: 'flex', alignItems: 'center',justifyContent: 'center'}}>
+           <div style={{marginTop: '20px',display: 'flex',justifyContent: 'center'}}>
                <Box 
-                   height={200}
-                   width={200}
+                   height={150}
+                   width={150}
                    my={4}
                    mx={4}
                    gap={4}
@@ -31,14 +32,14 @@ export default function MyAccount(){
                >
                 <div>
                     <IconButton onClick={() => navigate('/myaccount/manage-profile') }>
-                        <PersonIcon  sx={{fontSize: 200,color: '#fff'}}/>
+                        <PersonIcon  sx={{fontSize: 150,color: '#17252A'}}/>
                     </IconButton>
                     <p style={{fontWeight: 'bold'}}>Profile</p>
                 </div>
                </Box> 
                <Box 
-                   height={200}
-                   width={200}
+                   height={150}
+                   width={150}
                    my={4}
                    mx={4}
                    gap={4}
@@ -52,14 +53,14 @@ export default function MyAccount(){
                >
                 <div>
                     <IconButton onClick={() => navigate("/myaccount/manage-address")}>
-                        <HomeIcon  sx={{fontSize: 200,color: '#fff'}} />
+                        <HomeIcon  sx={{fontSize: 150,color: '#17252A'}} />
                     </IconButton>
                     <p style={{fontWeight: 'bold'}}>Address</p>
                 </div>
                </Box> 
                <Box 
-                   height={200}
-                   width={200}
+                   height={150}
+                   width={150}
                    my={4}
                    mx={4}
                    gap={4}
@@ -72,10 +73,75 @@ export default function MyAccount(){
                    
                >
                 <div>
-                    <IconButton onClick={() => navigate("/myaccount/view-education")}>
-                        <SchoolIcon  sx={{fontSize: 200,color: '#fff'}} />
+                    <IconButton onClick={() => navigate("/myaccount/manage-education")}>
+                        <SchoolIcon  sx={{fontSize: 150,color: '#17252A'}} />
                     </IconButton>
-                    <p style={{fontWeight: 'bold'}}>Education</p>
+                    <p style={{fontWeight: 'bold'}}>Qualification</p>
+                </div>
+               </Box> 
+           </div>
+           <div style={{display: 'flex',justifyContent: 'center'}}>
+               <Box    
+                   height={150}
+                   width={150}
+                   my={4}
+                   mx={4}
+                   gap={4}
+                   p={4}
+                   display='flex'
+                   alignItems='center'
+                   justifyContent='center'
+                   textAlign='center'
+                   bgcolor="#17252A"
+
+               >
+                <div>
+                    <IconButton>
+                        <PsychologyIcon  sx={{fontSize: 150,color: '#fff'}}/>
+                    </IconButton>
+                    <p style={{fontWeight: 'bold',color: '#fff'}}>Skills</p>
+                </div>
+               </Box> 
+               <Box 
+                   height={150}
+                   width={150}
+                   my={4}
+                   mx={4}
+                   gap={4}
+                   p={4}
+                   display='flex'
+                   alignItems='center'
+                   justifyContent='center'
+                   textAlign='center'
+                   bgcolor="#17252A"
+                   
+               >
+                <div>
+                    <IconButton>
+                        <WorkIcon  sx={{fontSize: 150,color: '#fff'}} />
+                    </IconButton>
+                    <p style={{fontWeight: 'bold',color: '#fff'}}>Work Experience</p>
+                </div>
+               </Box> 
+               <Box 
+                   height={150}
+                   width={150}
+                   my={4}
+                   mx={4}
+                   gap={4}
+                   p={4}
+                   display='flex'
+                   alignItems='center'
+                   justifyContent='center'
+                   textAlign='center'
+                   bgcolor="#17252A"
+                   
+               >
+                <div>
+                    <IconButton>
+                        <LanguageIcon  sx={{fontSize: 150,color: '#fff'}} />
+                    </IconButton>
+                    <p style={{fontWeight: 'bold',color: '#fff'}}>Online Accounts</p>
                 </div>
                </Box> 
            </div>
